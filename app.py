694bd22b2da6abba08503793862d669e16c6e647
Russell Cullen <russellscullen@gmail.com>
2012-12-13 17:21:37 -0500

    Added list of gifs

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import tornado.ioloop
 import tornado.web
+import tornado.template
 import os
+
+base = "http://gif-picker.herokuapp.com/static/gifs/"
+
+test = []
+
+for root, dirs, files in os.walk('static/'):
+	for f in files:
+		test.append(os.path.join(root, f))
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Howdy")
+        self.write(loader.load("home.html").generate(title="Home", dirs=test))
 
 settings = {'static_path': os.path.join(os.path.dirname(__file__), "static")}
 
@@ -14,5 +23,6 @@ application = tornado.web.Application([
 
 if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 5000))
+	loader = tornado.template.Loader("./")
 	application.listen(port)
 	tornado.ioloop.IOLoop.instance().start()
