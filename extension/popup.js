f136f2d1e7e6628495382a472d2942408302a68c
Russell Cullen <russellscullen@gmail.com>
2012-12-13 17:26:34 -0500

    new popup

--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,10 @@
-window.onload = showGifs
+
+var req = new XMLHttpRequest()
+req.open("GET", "http://gif-picker.herokuapp.com")
+req.onload = show
+req.send(null)
+
+// window.onload = showGifs
 
 function showGifs() {
   for (var i = 0; i < 10; i++) {
@@ -6,4 +12,8 @@ function showGifs() {
     img.src = "http://gif-picker.herokuapp.com/static/gifs/awesome/mind-blow.gif";
     document.body.appendChild(img);
   }
+}
+
+function show() {
+	document.write(req.response)
 }
