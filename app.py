8088afa06a3122a406de94117a9e6791502c60ea
Russell Cullen <russellscullen@gmail.com>
2012-12-13 20:37:39 -0500

    Added directory structure

--- app.py
+++ app.py
@@ -3,22 +3,29 @@ import tornado.web
 import tornado.template
 import os
 
-base = "http://gif-picker.herokuapp.com/static/gifs/"
+ds = []
 
-test = []
-
-for root, dirs, files in os.walk('static/'):
-	for f in files:
-		test.append(os.path.join(root, f))
+for root, dirs, files in os.walk('static/gifs/'):
+	for d in dirs:
+		ds.append(os.path.join(root, d))
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write(loader.load("home.html").generate(title="Home", dirs=test))
+        self.write(loader.load("dir.html").generate(title="Home", dirs=ds))
+
+class GifHandler(tornado.web.RequestHandler):
+    def get(self, slug):
+    	g = []
+    	for root, dirs, files in os.walk(slug):
+    		for f in files:
+    			g.append(os.path.join(root,f))
+        self.write(loader.load("home.html").generate(title="Home", dirs=g))
 
 settings = {'static_path': os.path.join(os.path.dirname(__file__), "static")}
 
 application = tornado.web.Application([
-	(r"/", MainHandler)
+	(r"/", MainHandler),
+	(r"/dir/(.*)", GifHandler)
 ], **settings)
 
 if __name__ == "__main__":
