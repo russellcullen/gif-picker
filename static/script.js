b4fd004d16726db8f23b9d609de3e345bc6898e7
Russell Cullen <russellscullen@gmail.com>
2012-12-19 05:02:43 -0500

    Add toast

--- static/script.js
+++ static/script.js
@@ -2,11 +2,19 @@ $(document).ready(function() {
 	$("#menu").accordion({	
 		collapsible: true, 
 		active: false,
+		heightStyle: "content",
 		activate: function(event, ui) {
 			$('.gif').zclip({
 				path:'/static/ZeroClipboard.swf',
 				copy: function() { return $(this).attr('src')},
-				afterCopy: function(){}
+				afterCopy: function() {
+					$.toast({
+				        message:"Copied!",
+				        displayTime:1000,
+				        inTime:100,
+				        outTime:200
+				    });
+				}
 			});
 		}
 	});
