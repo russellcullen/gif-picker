1a0bcbe30ae882784573a953abee93ce570b1548
Russell Cullen <russellscullen@gmail.com>
2012-12-20 01:39:43 -0500

    lazy load gifs

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
+	$("img").lazyload({event: "showing"});
 	$("#menu").accordion({	
 		collapsible: true, 
 		active: false,
 		heightStyle: "content",
 		activate: function(event, ui) {
 			if (ui.newPanel) {
+				ui.newPanel.children().trigger("showing");
 				ui.newPanel.children().zclip({
 					path:'/static/js/ZeroClipboard.swf',
-					copy: function() { return $(this).attr('src')},
+					copy: function() { return $(this).attr('data-original')},
 					afterCopy: function() {
 						$.toast({
 					        message:"Copied!",
