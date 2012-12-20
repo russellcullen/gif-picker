6134dff0835071b3b33e2609df600577eb00d172
Russell Cullen <russellscullen@gmail.com>
2012-12-19 21:11:21 -0500

    zclip tinkering

--- static/js/script.js
+++ static/js/script.js
@@ -4,18 +4,20 @@ $(document).ready(function() {
 		active: false,
 		heightStyle: "content",
 		activate: function(event, ui) {
-			$('.gif').zclip({
-				path:'/static/ZeroClipboard.swf',
-				copy: function() { return $(this).attr('src')},
-				afterCopy: function() {
-					$.toast({
-				        message:"Copied!",
-				        displayTime:1000,
-				        inTime:100,
-				        outTime:200
-				    });
-				}
-			});
+			if (ui.newPanel) {
+				ui.newPanel.children().zclip({
+					path:'/static/js/ZeroClipboard.swf',
+					copy: function() { return $(this).attr('src')},
+					afterCopy: function() {
+						$.toast({
+					        message:"Copied!",
+					        displayTime:1000,
+					        inTime:100,
+					        outTime:200
+					    });
+					}
+				});
+			}
 		}
 	});
 });
